5b429648098ad0d98bac39025f8ce1343660c6fe
leaftha <vltpcks@gmail.com>
2023-07-17 14:39:58 +0900

    분류

---
+++ programmers/입문/배열의 평균값.js
@@ -0,0 +1,14 @@
+// 문제 설명
+// 정수 배열 numbers가 매개변수로 주어집니다.
+// numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+    var answer = 0;
+    let count = 0;
+    numbers.map((n) => {
+        return (count += n);
+    });
+
+    answer = count / numbers.length;
+    return answer;
+}
