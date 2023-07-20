1012ae2ed6f88d7244ede237871b83d827c0cfb7
leaftha <vltpcks@gmail.com>
2023-07-20 10:04:11 +0900

    Create 구슬을 나누는 경우의 수.js

---
+++ programmers/입문/구슬을 나누는 경우의 수.js
@@ -0,0 +1,19 @@
+// 문제 설명
+// 머쓱이는 구슬을 친구들에게 나누어주려고 합니다.
+// 구슬은 모두 다르게 생겼습니다. 머쓱이가 갖고 있는
+// 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이
+// 매개변수로 주어질 때, balls개의 구슬 중 share개의 구슬을 고르는
+// 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+
+function solution(balls, share) {
+    var answer = factorial(balls) / (factorial(balls - share) * factorial(share));
+    return Math.round(answer);
+}
+
+function factorial(num) {
+    if (num < 0) return -1;
+    else if (num == 0) return 1;
+    else {
+        return num * factorial(num - 1);
+    }
+}
