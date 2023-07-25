b6ed15178c121a1ba12fee755a3e2945fae1374f
leaftha <vltpcks@gmail.com>
2023-07-25 09:35:22 +0900

    Update 영어가 싫어요.js

--- programmers/입문/영어가 싫어요.js
+++ programmers/입문/영어가 싫어요.js
@@ -1,3 +1,7 @@
+// 문제 설명
+// 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다.
+// 문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+
 function solution(numbers) {
     var answer = 0;
     let str = '';
