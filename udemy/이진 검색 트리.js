063db9781d6040d3b68d3ed1542a81bb49101eb9
leaftha <vltpcks@gmail.com>
2023-07-26 14:05:44 +0900

    tree insert method

--- udemy/이진 검색 트리.js
+++ udemy/이진 검색 트리.js
@@ -52,6 +52,31 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(val) {
+        var newNode = new Node(val);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while (true) {
+            if (val === current.val) return undefined;
+            if (val < current.val) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
 }
 
 let tree = new BinarySearchTree();
