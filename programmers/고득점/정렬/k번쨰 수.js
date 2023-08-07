8609d8f73241981bc806fec01a3d5b4a90b68bf6
SaChan <vltpcks@gmail.com>
2023-08-07 21:10:41 +0900

    chanage

--- programmers/고득점/정렬/k번쨰 수.js
+++ programmers/고득점/정렬/k번쨰 수.js
@@ -10,16 +10,16 @@
 // commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
 
 function solution(array, commands) {
-    var answer = [];
-    for (let i = 0; i < commands.length; i++) {
-        let arr = [...array];
-        let n = commands[i][1] - commands[i][0] + 1;
-        let arr2 = arr.splice(commands[i][0] - 1, n);
-        arr2.sort((a, b) => {
-            return a - b;
-        });
-        let num = commands[i][2] - 1;
-        answer.push(arr2[num]);
-    }
-    return answer;
+  var answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    let arr = [...array];
+    let n = commands[i][1] - commands[i][0] + 1;
+    let arr2 = arr.splice(commands[i][0] - 1, n);
+    arr2.sort((a, b) => {
+      return a - b;
+    });
+    let num = commands[i][2] - 1;
+    answer.push(arr2[num]);
+  }
+  return answer;
 }
