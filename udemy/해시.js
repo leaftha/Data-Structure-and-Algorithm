5af8d975408f6f26651f35f984bf5a53ba63c6f7
leaftha <vltpcks@gmail.com>
2023-08-11 11:17:48 +0900

    hash tabel key and values method

--- udemy/해시.js
+++ udemy/해시.js
@@ -80,6 +80,7 @@ class HashTable {
         }
         return total;
     }
+
     set(key, value) {
         let index = this._hash(key);
         if (!this.keyMap[index]) {
@@ -87,6 +88,7 @@ class HashTable {
         }
         this.keyMap[index].push([key, value]);
     }
+
     get(key) {
         let index = this._hash(key);
         if (this.keyMap[index]) {
@@ -97,6 +99,34 @@ class HashTable {
             }
         }
         return undefined;
+    }
+
+    keys() {
+        let keysArr = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!keysArr.includes(this.keyMap[i][j][0])) {
+                        keysArr.push(this.keyMap[i][j][0]);
+                    }
+                }
+            }
+        }
+        return keysArr;
+    }
+
+    values() {
+        let valuesArr = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!valuesArr.includes(this.keyMap[i][j][1])) {
+                        valuesArr.push(this.keyMap[i][j][1]);
+                    }
+                }
+            }
+        }
+        return valuesArr;
     }
 }
 
