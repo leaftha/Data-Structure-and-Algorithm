77f6465d3437a84325c10d5e3cf7066a97c04f14
SaChan <vltpcks@gmail.com>
2023-11-27 22:09:23 +0900

    Update 다익스트라.js

--- udemy/다익스트라.js
+++ udemy/다익스트라.js
@@ -7,70 +7,70 @@
 // 네비게이션의 최단 길 찾기등에 사용
 
 class WeightedGraph {
-    constructor() {
-        this.adjacencyList = {};
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2, weight) {
+    this.adjacencyList[v1].push({ node: v2, weight });
+    this.adjacencyList[v2].push({ node: v1, weight });
+  }
+
+  Dijkstra(start, finish) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+    let path = [];
+    let smallest;
+    // 순정 상태
+    for (let vertex in this.adjacencyList) {
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
+      }
+      previous[vertex] = null;
     }
 
-    addVertex(vertex) {
-        if (!this.adjacencyList[vertex]) {
-            this.adjacencyList[vertex] = [];
+    while (nodes.values.length) {
+      smallest = nodes.dequeue().val;
+      if (smallest === finish) {
+        while (previous[smallest]) {
+          path.push(smallest);
+          smallest = previous[smallest];
         }
-    }
+        break;
+      }
 
-    addEdge(v1, v2, weight) {
-        this.adjacencyList[v1].push({ node: v2, weight });
-        this.adjacencyList[v2].push({ node: v1, weight });
-    }
+      if (smallest || distances[smallest] !== Infinity) {
+        for (let neighbor in this.adjacencyList[smallest]) {
+          // 인접점 찾기
+          let nextNode = this.adjacencyList[smallest][neighbor];
+          // console.log(nextNode)
+          // 인접점의 거리 최신화
+          let candidate = distances[smallest] + nextNode.weight;
+          console.log(candidate, smallest, nextNode.node);
+          let nextNeighbor = nextNode.node;
 
-    Dijkstra(start, finish) {
-        const nodes = new PriorityQueue();
-        const distances = {};
-        const previous = {};
-        let path = [];
-        let smallest;
-        // 순정 상태
-        for (let vertex in this.adjacencyList) {
-            if (vertex === start) {
-                distances[vertex] = 0;
-                nodes.enqueue(vertex, 0);
-            } else {
-                distances[vertex] = Infinity;
-                nodes.enqueue(vertex, Infinity);
-            }
-            previous[vertex] = null;
+          if (candidate < distances[nextNeighbor]) {
+            distances[nextNeighbor] = candidate;
+            previous[nextNeighbor] = smallest;
+            nodes.enqueue(nextNeighbor, candidate);
+          }
         }
-
-        while (nodes.values.length) {
-            smallest = nodes.dequeue().val;
-            if (smallest === finish) {
-                while (previous[smallest]) {
-                    path.push(smallest);
-                    smallest = previous[smallest];
-                }
-                break;
-            }
-
-            if (smallest || distances[smallest] !== Infinity) {
-                for (let neighbor in this.adjacencyList[smallest]) {
-                    // 인접점 찾기
-                    let nextNode = this.adjacencyList[smallest][neighbor];
-                    // console.log(nextNode)
-                    // 인접점의 거리 최신화
-                    let candidate = distances[smallest] + nextNode.weight;
-                    console.log(candidate, smallest, nextNode.node);
-                    let nextNeighbor = nextNode.node;
-
-                    if (candidate < distances[nextNeighbor]) {
-                        distances[nextNeighbor] = candidate;
-                        previous[nextNeighbor] = smallest;
-                        nodes.enqueue(nextNeighbor, candidate);
-                    }
-                }
-            }
-        }
-
-        return path.concat(smallest).reverse();
+      }
     }
+
+    return path.concat(smallest).reverse();
+  }
 }
 
 //*************************************************************
@@ -81,40 +81,40 @@ class WeightedGraph {
 // 우선순위 큐
 
 class PriorityQueue {
-    constructor() {
-        this.values = [];
-    }
-    enqueue(val, priority) {
-        this.values.push({ val, priority });
-        this.sort();
-    }
-    dequeue() {
-        return this.values.shift();
-    }
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
 
-    sort() {
-        this.values.sort((a, b) => a.priority - b.priority);
-    }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
 }
 
 const g = new WeightedGraph();
 
-g.addVertex('A');
-g.addVertex('B');
-g.addVertex('C');
-g.addVertex('D');
-g.addVertex('E');
-g.addVertex('F');
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
 
-g.addEdge('A', 'B', 4);
-g.addEdge('A', 'C', 2);
-g.addEdge('B', 'E', 3);
-g.addEdge('C', 'D', 2);
-g.addEdge('C', 'F', 4);
-g.addEdge('D', 'E', 3);
-g.addEdge('D', 'F', 1);
-g.addEdge('E', 'F', 1);
+g.addEdge("A", "B", 4);
+g.addEdge("A", "C", 2);
+g.addEdge("B", "E", 3);
+g.addEdge("C", "D", 2);
+g.addEdge("C", "F", 4);
+g.addEdge("D", "E", 3);
+g.addEdge("D", "F", 1);
+g.addEdge("E", "F", 1);
 
 console.log(g);
 
-g.Dijkstra('A', 'E');
+g.Dijkstra("A", "E");
