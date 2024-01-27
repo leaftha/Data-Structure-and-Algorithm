2140662eba584584cd8d0daef52a0903fbab0c49
SaChan <vltpcks@gmail.com>
2024-01-27 21:16:56 +0900

    Create 전화번호 목록(#).js

---
+++ programmers/고득점/해시/전화번호 목록(#).js
@@ -0,0 +1,16 @@
+function solution(phoneBook) {
+  const table = {};
+
+  for (const number of phoneBook) {
+    table[number] = true;
+  }
+
+  for (const number of phoneBook) {
+    for (let i = 1; i < number.length; i += 1) {
+      const prefix = number.slice(0, i);
+      if (table[prefix]) return false;
+    }
+  }
+
+  return true;
+}
