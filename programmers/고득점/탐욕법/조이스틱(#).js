705d0b6b0a61945b004f9d9db3afd9138db0461b
leaftha <vltpcks@gmail.com>
2024-02-20 10:35:20 +0900

    Create 조이스틱(#).js

---
+++ programmers/고득점/탐욕법/조이스틱(#).js
@@ -0,0 +1,27 @@
+function solution(name) {
+    var answer = 0;
+    let min = name.length - 1;
+
+    for (let i = 0; i < name.length; i++) {
+        let currentAlphabet = name.charCodeAt(i);
+
+        if (currentAlphabet < 78) {
+            answer += currentAlphabet % 65;
+        } else {
+            answer += 91 - currentAlphabet;
+        }
+
+        let nextIndex = i + 1;
+
+        while (nextIndex < name.length && name.charCodeAt(nextIndex) === 65) {
+            nextIndex += 1;
+        }
+        min = Math.min(
+            min,
+            i * 2 + name.length - nextIndex, // 먼저 오른쪽으로 가기
+            i + (name.length - nextIndex) * 2 // 처음부터 반대로 가기
+        );
+    }
+    answer += min;
+    return answer;
+}
