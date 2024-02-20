017f2186696fbb48c9607e9d9ae43d82cf7ea360
leaftha <vltpcks@gmail.com>
2024-02-20 10:35:06 +0900

    Update 124 나라의 숫자(#).js

--- programmers/lv2/124 나라의 숫자(#).js
+++ programmers/lv2/124 나라의 숫자(#).js
@@ -3,11 +3,9 @@ function solution(n) {
     let stack = [];
     while (n > 0) {
         if (n % 3 === 0) {
-            // 나머지가 0이면 나머지를 4로 바꿔주고 몫에서 -1.
             stack.push(4);
             n = Math.floor(n / 3) - 1;
         } else {
-            // 나머지가 0이 아닌 경우 3진법으로 계산.
             stack.push(n % 3);
             n = Math.floor(n / 3);
         }
