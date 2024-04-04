d80777b613e12bf10da171fcf73176a52fa83978
leaftha <vltpcks@gmail.com>
2024-04-04 10:25:43 +0900

    Create 점찍기(#).js

---
+++ programmers/lv2/점찍기(#).js
@@ -0,0 +1,10 @@
+function solution(k, d) {
+    var answer = 0;
+
+    for (let x = 0; x <= d; x += k) {
+        const max_y = parseInt(Math.sqrt(d ** 2 - x ** 2));
+        answer += parseInt(max_y / k) + 1;
+    }
+
+    return answer;
+}
