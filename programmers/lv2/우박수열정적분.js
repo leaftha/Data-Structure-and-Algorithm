62520ff881f0d057241e4ed6ff5d8355ea140fa3
leaftha <vltpcks@gmail.com>
2024-04-11 16:42:36 +0900

    Create 우박수열정적분.js

---
+++ programmers/lv2/우박수열정적분.js
@@ -0,0 +1,16 @@
+function solution(k, ranges) {
+    const integral = [0];
+    while (k !== 1) {
+        if (k % 2) {
+            integral.push((k + k * 3 + 1) / 2 + integral.at(-1));
+            k = k * 3 + 1;
+        } else {
+            integral.push((k * 3) / 4 + integral.at(-1));
+            k /= 2;
+        }
+    }
+    return ranges.map(([s, e]) => {
+        if (integral.length - 1 + e < s) return -1;
+        return integral.at(e - 1) - integral[s];
+    });
+}
