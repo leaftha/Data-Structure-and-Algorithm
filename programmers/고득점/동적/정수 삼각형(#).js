12f13bdbac4edb91f035b52031580a414ab8edd6
leaftha <vltpcks@gmail.com>
2024-05-02 15:17:35 +0900

    change folder

--- programmers/고득점/동적/정수 삼각형(#).js
+++ programmers/고득점/동적/정수 삼각형(#).js
@@ -18,25 +18,25 @@
 // 재귀를 dp식으로 바꾸는 연습 필요
 
 function solution(triangle) {
-  var answer = 0;
-  let length = triangle.length;
-  let dp = Array.from(Array(length), () => Array());
+    var answer = 0;
+    let length = triangle.length;
+    let dp = Array.from(Array(length), () => Array());
 
-  function recur(n, t) {
-    if (dp[t][n] != undefined) {
-      return dp[t][n];
+    function recur(n, t) {
+        if (dp[t][n] != undefined) {
+            return dp[t][n];
+        }
+
+        if (t === length - 1) {
+            return triangle[t][n];
+        }
+        dp[t][n] = triangle[t][n] + Math.max(recur(n + 1, t + 1), recur(n, t + 1));
+
+        return dp[t][n];
     }
+    recur(0, 0);
 
-    if (t === length - 1) {
-      return triangle[t][n];
-    }
-    dp[t][n] = triangle[t][n] + Math.max(recur(n + 1, t + 1), recur(n, t + 1));
+    answer = dp[0][0];
 
-    return dp[t][n];
-  }
-  recur(0, 0);
-
-  answer = dp[0][0];
-
-  return answer;
+    return answer;
 }
