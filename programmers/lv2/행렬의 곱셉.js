4ffb6db8047d933e548fe30e2faadf260f960a58
leaftha <vltpcks@gmail.com>
2024-07-08 13:12:00 +0900

    책 다시풀기

--- programmers/lv2/행렬의 곱셉.js
+++ programmers/lv2/행렬의 곱셉.js
@@ -7,18 +7,38 @@
 // 곱할 수 있는 배열만 주어집니다.
 
 function solution(arr1, arr2) {
-    const answer = [];
+  const answer = [];
 
-    for (let i = 0; i < arr1.length; i++) {
-        let result = [];
-        for (let j = 0; j < arr2[0].length; j++) {
-            let elem = 0;
-            for (let k = 0; k < arr2.length; k++) {
-                elem += arr1[i][k] * arr2[k][j];
-            }
-            result.push(elem);
-        }
-        answer.push(result);
+  for (let i = 0; i < arr1.length; i++) {
+    let result = [];
+    for (let j = 0; j < arr2[0].length; j++) {
+      let elem = 0;
+      for (let k = 0; k < arr2.length; k++) {
+        elem += arr1[i][k] * arr2[k][j];
+      }
+      result.push(elem);
     }
-    return answer;
+    answer.push(result);
+  }
+  return answer;
+}
+
+// ------------------------------------------------------------------
+
+function solution(arr1, arr2) {
+  var answer = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    answer.push(new Array(arr2[0].length).fill(0));
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2[0].length; j++) {
+      for (let k = 0; k < arr2.length; k++) {
+        answer[i][j] += arr1[i][k] * arr2[k][j];
+      }
+    }
+  }
+
+  return answer;
 }
