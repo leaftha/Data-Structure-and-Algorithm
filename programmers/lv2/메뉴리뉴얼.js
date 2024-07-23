884a1843634deb997adbc8962c2f3c2079706d71
leaftha <vltpcks@gmail.com>
2024-07-23 13:28:21 +0900

    다시 풀기

--- programmers/lv2/메뉴리뉴얼.js
+++ programmers/lv2/메뉴리뉴얼.js
@@ -33,3 +33,46 @@ const Combinations = (arr, num) => {
 
   return results;
 };
+
+// -------------------------------------------------------------------------
+function combinations(arr, n) {
+  if (n === 1) return arr.map((v) => [v]);
+  const result = [];
+
+  arr.forEach((fixed, idx, arr) => {
+    const rest = arr.slice(idx + 1);
+    const combis = combinations(rest, n - 1);
+    const combins = combis.map((v) => [fixed, ...v]);
+    result.push(...combins);
+  });
+  return result;
+}
+
+function solution(orders, course) {
+  var answer = [];
+
+  for (const c of course) {
+    const menu = [];
+    for (const order of orders) {
+      const orderArr = order.split("").sort();
+      const comb = combinations(orderArr, c);
+      menu.push(...comb);
+    }
+    const counter = {};
+    for (const m of menu) {
+      const key = m.join("");
+      counter[key] = (counter[key] || 0) + 1;
+    }
+
+    const max = Math.max(...Object.values(counter));
+    if (max > 1) {
+      for (let [key, value] of Object.entries(counter)) {
+        if (value === max) {
+          answer.push(key);
+        }
+      }
+    }
+  }
+  answer.sort();
+  return answer;
+}
