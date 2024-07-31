e6ca03f07022e6aa57fe5ff86ff6578d4e33851b
SaChan <vltpcks@gmail.com>
2024-07-31 16:17:37 +0900

    다시 풀기

--- programmers/고득점/DFS/BFS/네트워크.js
+++ programmers/고득점/DFS/BFS/네트워크.js
@@ -1,35 +1,59 @@
 function solution(n, computers) {
-    var answer = 0;
-    let obj = {};
-    let vis = Array(n).fill(0);
+  var answer = 0;
+  let obj = {};
+  let vis = Array(n).fill(0);
 
-    for (let i = 0; i < n; i++) {
-        obj[i] = [];
+  for (let i = 0; i < n; i++) {
+    obj[i] = [];
+  }
+
+  for (let i = 0; i < computers.length; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i != j && computers[i][j] === 1) {
+        obj[i].push(j);
+      }
     }
+  }
 
-    for (let i = 0; i < computers.length; i++) {
-        for (let j = 0; j < n; j++) {
-            if (i != j && computers[i][j] === 1) {
-                obj[i].push(j);
-            }
+  for (let i = 0; i < n; i++) {
+    if (vis[i] === 1) continue;
+    let q = [i];
+    vis[i] = 1;
+    answer++;
+    while (q.length) {
+      let L = q.shift();
+      if (obj[L].length != 0) {
+        for (let x of obj[L]) {
+          if (vis[x] === 1) continue;
+          q.push(x);
+          vis[x] = 1;
         }
+      }
     }
+  }
+  return answer;
+}
 
-    for (let i = 0; i < n; i++) {
-        if (vis[i] === 1) continue;
-        let q = [i];
-        vis[i] = 1;
-        answer++;
-        while (q.length) {
-            let L = q.shift();
-            if (obj[L].length != 0) {
-                for (let x of obj[L]) {
-                    if (vis[x] === 1) continue;
-                    q.push(x);
-                    vis[x] = 1;
-                }
-            }
-        }
+// -------------------------------------------------------
+
+function dfs(computers, visted, node) {
+  visted[node] = true;
+  for (let i = 0; i < computers[node].length; i++) {
+    if (computers[node][i] && !visted[i]) {
+      dfs(computers, visted, i);
     }
-    return answer;
+  }
+}
+
+function solution(n, computers) {
+  var answer = 0;
+  const visted = Array(n).fill(false);
+
+  for (let i = 0; i < n; i++) {
+    if (!visted[i]) {
+      dfs(computers, visted, i);
+      answer++;
+    }
+  }
+  return answer;
 }
