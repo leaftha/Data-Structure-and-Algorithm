cb32e7116b0df47424a074b6570ceaec755e62c5
SaChan <vltpcks@gmail.com>
2024-08-02 21:22:25 +0900

    다시 풀기

--- programmers/lv2/전력망을 둘로 나누기.js
+++ programmers/lv2/전력망을 둘로 나누기.js
@@ -40,3 +40,39 @@ function solution(n, wires) {
 
   return answer;
 }
+
+// -----------------------------------------------------------
+
+function solution(n, wires) {
+  const graph = Array.from({ length: n + 1 }, () => []);
+  for (const [a, b] of wires) {
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+
+  function dfs(node, parent) {
+    let cnt = 1;
+    for (const child of graph[node]) {
+      if (child !== parent) {
+        cnt += dfs(child, node);
+      }
+    }
+    return cnt;
+  }
+
+  let min = Infinity;
+
+  for (const [a, b] of wires) {
+    graph[a].splice(graph[a].indexOf(b), 1);
+    graph[b].splice(graph[b].indexOf(a), 1);
+
+    const cntA = dfs(a, b);
+    const cntB = n - cntA;
+
+    min = Math.min(min, Math.abs(cntA - cntB));
+
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  return min;
+}
