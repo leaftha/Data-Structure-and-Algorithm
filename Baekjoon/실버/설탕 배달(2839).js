b7b4a930802220d1896a2753314726c481890b1d
SaChan <vltpcks@gmail.com>
2024-11-10 12:55:08 +0900

    Update 설탕 배달(2839).js

--- Baekjoon/실버/설탕 배달(2839).js
+++ Baekjoon/실버/설탕 배달(2839).js
@@ -18,3 +18,13 @@ while (n > 0) {
 }
 
 console.log(n === 0 ? answer : -1);
+
+const dp = Array(n + 1).fill(Infinity);
+dp[0] = 0;
+
+for (let i = 3; i <= n; i++) {
+  if (i >= 3) dp[i] = Math.min(dp[i], dp[i - 3] + 1);
+  if (i >= 5) dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+}
+
+console.log(dp[n] === Infinity ? -1 : dp[n]);
