5ae2d4b1f7bd6925858b3f38cd10d920258ccc7f
SaChan <vltpcks@gmail.com>
2025-03-19 21:35:10 +0900

    Create ATM(11399).js

---
+++ Baekjoon/실버/ATM(11399).js
@@ -0,0 +1,8 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.trim().split(" ").map(Number));
+
+let [n] = input.shift();
