d676fcee827af0a314d239cfb43e0fecc0ccb193
SaChan <vltpcks@gmail.com>
2025-03-19 21:39:38 +0900

    Update ATM(11399).js

--- Baekjoon/실버/ATM(11399).js
+++ Baekjoon/실버/ATM(11399).js
@@ -6,3 +6,22 @@ const input = require("fs")
   .map((el) => el.trim().split(" ").map(Number));
 
 let [n] = input.shift();
+
+let arr = input.shift();
+arr.sort((a, b) => {
+  return a - b;
+});
+
+let dp = Array(n).fill(0);
+dp[0] = arr[0];
+
+for (let i = 1; i < n; i++) {
+  dp[i] = dp[i - 1] + arr[i];
+}
+
+let answer = 0;
+for (let num of dp) {
+  answer += num;
+}
+
+console.log(answer);
