61badbf509de8a24ee72aa5c323770207347fea4
SaChan <vltpcks@gmail.com>
2025-08-03 20:47:53 +0900

    Create 숫자고르기(2668).js

---
+++ Baekjoon/골드/숫자고르기(2668).js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.trim().split(" ").map(Number));
+
+let [n] = input.shift();
+let arr = [];
+
+for (let [i] of input) {
+  arr.push(i);
+}
+arr.unshift(0);
+let result = [];
+const dfs = (start, cur, path, vistied) => {
+  if (vistied[cur]) {
+    if (start === cur) {
+      result.push(...path);
+    }
+    return;
+  }
+  vistied[cur] = true;
+  path.push(cur);
+  dfs(start, arr[cur], path, vistied);
+};
+
+for (let i = 1; i < arr.length; i++) {
+  const vistied = Array(n + 1).fill(false);
+  dfs(i, i, [], vistied);
+}
+
+const answer = Array.from(new Set(result)).sort((a, b) => a - b);
+console.log(answer.length);
+console.log(answer.join("\n"));
