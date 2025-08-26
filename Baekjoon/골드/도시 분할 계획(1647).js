d57c68634fbc7356eab96c3409935dc5ba28c859
SaChan <vltpcks@gmail.com>
2025-08-26 20:38:13 +0900

    Create 도시 분할 계획(1647).js

---
+++ Baekjoon/골드/도시 분할 계획(1647).js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.trim().split(" ").map(Number));
+
+const [n, m] = input.shift();
+
+const edges = [];
+
+for (let [a, b, c] of input) {
+  edges.push([c, a, b]);
+}
+
+edges.sort((a, b) => a[0] - b[0]);
+
+const parent = Array.from({ length: n + 1 }, (_, i) => i);
+
+const rank = new Array(n + 1).fill(0);
+
+const find = (x) => {
+  while (parent[x] !== x) {
+    parent[x] = parent[parent[x]];
+    x = parent[x];
+  }
+  return x;
+};
+
+const union = (a, b) => {
+  a = find(a);
+  b = find(b);
+
+  if (a === b) return false;
+  if (rank[a] < rank[b]) {
+    parent[a] = b;
+  } else if (rank[a] > rank[b]) {
+    parent[b] = a;
+  } else {
+    parent[b] = a;
+    rank[a]++;
+  }
+  return true;
+};
+
+let sum = 0;
+let maxEdge = 0;
+let picked = 0;
+
+for (const [c, a, b] of edges) {
+  if (union(a, b)) {
+    sum += c;
+    if (c > maxEdge) maxEdge = c;
+    picked++;
+    if (picked === n - 1) break;
+  }
+}
+
+console.log(parent);
+console.log(rank);
+console.log(sum - maxEdge);
