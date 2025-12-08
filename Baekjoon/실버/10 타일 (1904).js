4c20e5398e770b08274eab1fd77e880360996652
SaChan <vltpcks@gmail.com>
2025-12-08 21:44:45 +0900

    Create 10 타일 (1904).js

---
+++ Baekjoon/실버/10 타일 (1904).js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.trim().split(" ").map(Number));
+
+let [n] = input.shift();
+
+const dp = Array(n + 1).fill(0);
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= n; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+}
+
+console.log(dp[n]);
