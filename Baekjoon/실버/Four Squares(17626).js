2b39f6055b392d650bdf24bfe9f4ffa1e60cc4ae
leaftha <vltpcks@gmail.com>
2026-02-03 13:57:20 +0900

    Create Four Squares(17626).js

---
+++ Baekjoon/실버/Four Squares(17626).js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.trim().split(" ").map(Number));
+
+let [n] = input.shift();
+
+const dp = Array(n + 1).fill(Infinity);
+dp[0] = 0;
+dp[1] = 1;
+
+for (let i = 2; i <= n; i++) {
+  for (let j = 1; j * j <= i; j++) {
+    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+  }
+}
+
+console.log(dp[n]);
