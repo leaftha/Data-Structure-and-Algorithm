39a567bf779ac211f5ebe50e65e0c10dced46c23
leaftha <vltpcks@gmail.com>
2026-02-04 14:09:43 +0900

    Create 상자넣기(1965).js

---
+++ Baekjoon/실버/상자넣기(1965).js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.trim().split(" ").map(Number));
+
+let [n] = input.shift();
+const arr = input.shift();
+const dp = Array(n).fill(1);
+
+for (let i = 1; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (arr[i] > arr[j]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
+}
+
+console.log(Math.max(...dp));
